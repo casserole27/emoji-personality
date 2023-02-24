8865b8fd4b0644ec63be327eeb152903ea3d909b
C Lewis <casserole.lewis@gmail.com>
2023-02-24 09:28:08 -0500

    Project redesign
    Semantic HTML
    
    Updated CSS to reflect space emoji design and added responsiveness

---
+++ index.js
@@ -0,0 +1,45 @@
+const myEmojis = ["☀️", "🌙", "⭐️"]
+const emojiContainer = document.getElementById("emoji-container")
+const emojiInput = document.getElementById("emoji-input")
+const pushBtn = document.getElementById("push-btn")
+const unshiftBtn = document.getElementById("unshift-btn")
+const popBtn = document.getElementById("pop-btn")
+const shiftBtn = document.getElementById("shift-btn")
+
+function renderEmojis() {
+    emojiContainer.innerHTML = ""
+    for (let i = 0; i < myEmojis.length; i++) {
+        const emoji = document.createElement('span')
+        emoji.textContent = myEmojis[i]
+        emojiContainer.append(emoji)
+    }
+}
+
+renderEmojis()
+
+pushBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.push(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+unshiftBtn.addEventListener("click", function(){
+    if (emojiInput.value) {
+        myEmojis.unshift(emojiInput.value)
+        emojiInput.value = ""
+        renderEmojis()
+    }
+})
+
+popBtn.addEventListener("click", function() {
+    myEmojis.pop()
+    renderEmojis()
+})
+
+shiftBtn.addEventListener("click", function() {
+    myEmojis.shift()
+    renderEmojis()
+})
+
